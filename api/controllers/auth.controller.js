609d4fdcab2e1c5a577d6d4687c9e60c4474c0a1
okasha ahsan <107637690+okashasaif@users.noreply.github.com>
2024-02-04 04:50:25 +0500

    Sign up pages

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,5 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
-
 export const signup = async (req, res,next)=>{
    const { username, email, password}= req.body;
    const hashedPassword = bcryptjs.hashSync(password,10);
@@ -12,5 +11,4 @@ export const signup = async (req, res,next)=>{
     catch (error){
         next(error)
     }
-   
 };
