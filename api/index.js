9b3b9ca43761beb9bc892cfedf1566eab04f15e9
okasha ahsan <107637690+okashasaif@users.noreply.github.com>
2024-02-04 03:27:32 +0500

    test apis

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter  from './routes/user.route.js';
 dotenv.config();
 mongoose.connect(process.env.MONGO).then (() => {
     console.log('Connected to mongoDB');
@@ -12,3 +13,5 @@ app.listen(3000, ()=>{
     console.log('server is running on port 3000!!');
 }
 );
+
+app.use("/api/user", userRouter);
