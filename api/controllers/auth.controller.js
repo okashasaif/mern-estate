4ba3e52c5f23a1da596516ff20980057f8c0a982
okasha ahsan <107637690+okashasaif@users.noreply.github.com>
2024-02-04 16:19:52 +0500

    Redux

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -21,7 +21,7 @@ export const signin = async (req,res,next)=>{
         const validUser = await User.findOne({email});
         if(!validUser) return next(errorHandler(404, 'User not Found!'));
         const validPassword = bcryptjs.compareSync(password, validUser.password);
-        if(!validPassword) returnnext(errorHandler(401,'invalid password'));
+        if(!validPassword) return next(errorHandler(401,'Wrong credentials!'));
         const token = jwt.sign({ id: validUser._id}, process.env.JWT_SECRET);
         const { password: pass, ...rest} = validUser._doc;
         res.cookie('access_token',token,{ httpOnly: true}).status(200).json(rest);
