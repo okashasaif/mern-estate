12c5c9f2d0d34489d272be97ead317eb0eec60f1
okasha ahsan <107637690+okashasaif@users.noreply.github.com>
2024-02-07 22:44:20 +0500

    error

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -2,7 +2,6 @@ import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from '../utils/error.js';
 import jwt from 'jsonwebtoken';
-import { error } from 'console';
 export const signup = async (req, res,next)=>{
    const { username, email, password}= req.body;
    const hashedPassword = bcryptjs.hashSync(password,10);
--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter  from './routes/user.route.js';
+import cookieParser from 'cookie-parser';
 import authRouter  from './routes/auth.route.js';
 dotenv.config();
 mongoose.connect(process.env.MONGO).then (() => {
@@ -20,6 +21,7 @@ app.listen(3000, ()=>{
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+app.use(cookieParser());
 
 
 app.use((err,req,res,next)=>{
