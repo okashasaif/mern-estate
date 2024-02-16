a58c83e91fb07f6ebfbad153d0404e93bf739cea
okasha ahsan <107637690+okashasaif@users.noreply.github.com>
2024-02-16 16:03:08 +0500

    vercel.json

--- api/index.js
+++ api/index.js
@@ -5,6 +5,8 @@ import userRouter  from './routes/user.route.js';
 import cookieParser from 'cookie-parser';
 import authRouter  from './routes/auth.route.js';
 import listingRouter from './routes/listing.route.js';
+import path from 'path';
+
 dotenv.config();
 mongoose.connect(process.env.MONGO).then (() => {
     console.log('Connected to mongoDB');
@@ -12,7 +14,7 @@ mongoose.connect(process.env.MONGO).then (() => {
     console.log(err)
 });
 
-
+const __dirname = path.resolve();
 
 const app = express();
 app.use(express.json());
@@ -25,7 +27,11 @@ app.listen(3000, ()=>{
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use('/api/listing', listingRouter);
+app.use(express.static(path.join(__dirname, '/client/dist')));
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
 
 app.use((err,req,res,next)=>{
 
