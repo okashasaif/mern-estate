b582e3861ecfa7cc77ddf8a3262a3f16abb54de4
okasha ahsan <107637690+okashasaif@users.noreply.github.com>
2024-02-16 06:25:34 +0500

    render deploy

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter  from './routes/user.route.js';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 import authRouter  from './routes/auth.route.js';
 import listingRouter from './routes/listing.route.js';
 dotenv.config();
@@ -10,7 +11,9 @@ mongoose.connect(process.env.MONGO).then (() => {
     console.log('Connected to mongoDB');
 }).catch((err)=>{
     console.log(err)
-})
+});
+
+const __dirname = path.resolve();
 
 
 const app = express();
@@ -25,6 +28,11 @@ app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use('/api/listing', listingRouter);
 
+app.use(express.static(path.join(__dirname, './client/dist')));
+
+app.get('*', (req,res)=>{
+    res.sendFile(path.join(__dirname, './client', 'dist', 'index.html'));
+})
 
 app.use((err,req,res,next)=>{
 
